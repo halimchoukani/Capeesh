0d4bedff4bd84dba19e559f5d75d37f5cb4b760d
Halim Choukani <halim.choukani@gmail.com>
2023-12-04 17:19:27 +0100

    adding header style v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { StoreComponent } from './store/store.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotfoundComponent },
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
