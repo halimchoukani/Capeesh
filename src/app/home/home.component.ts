36ecb20531076af7918c223d7fbfe96e492cec01
Halim Choukani <halim.choukani@gmail.com>
2023-12-05 20:27:46 +0100

    Fix header component and add home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedDataService } from '../services/shared-data.service';
+import { of, filter } from 'rxjs';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  randomPhone: any;
+  promoPhones: any;
+  constructor(private _shared: SharedDataService) {
+    of(this._shared.phones).subscribe((phones: any[]) => {
+      const randomIndex = Math.floor(Math.random() * phones.length);
+      this.randomPhone = phones[randomIndex];
+    });
+    of(this._shared.phones).subscribe((phones: any[]) => {
+      this.promoPhones = phones.filter((phone: any) => phone.promo);
+    });
+  }
 }
---
+++ src/app/services/shared-data.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedDataService {
+  phones: any = [
+    {
+      id: 1,
+      name: 'iPhone 12 Pro Max',
+      price: 1099,
+      brand: 'Apple',
+      description: 'A large phone with one of the best screens',
+      image:
+        'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-max-blue-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021658000',
+      promo: true,
+    },
+    {
+      id: 2,
+      name: 'iPhone 12 Pro',
+      price: 999,
+      brand: 'Apple',
+      description: 'A great phone with one of the best cameras',
+      image:
+        'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021658000',
+      promo: false,
+    },
+    {
+      id: 3,
+      name: 'iPhone 12',
+      price: 699,
+      brand: 'Apple',
+      description: 'A great phone with one of the best cameras',
+      image:
+        'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-blue-select-2020?wid=940&hei=1112&fmt=png-alpha&.v=1604343704000',
+      promo: true,
+    },
+    {
+      id: 4,
+      name: 'iPhone 12 Mini',
+      price: 599,
+      brand: 'Apple',
+      description: 'A great phone with one of the best cameras',
+      image:
+        'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-mini-blue-select-2020?wid=940&hei=1112&fmt=png-alpha&.v=1604343704000',
+      promo: true,
+    },
+  ];
+  constructor() {}
+}
